ab8578e8630d6247e6f172baef4bd10736e32230
kitty14956590 <borbtrash@gmail.com>
2024-04-18 18:59:56 -0400

    definitions

---
+++ includes/integer.h
@@ -0,0 +1,5 @@
+int int_add(int x, int y);
+int int_sub(int x, int y);
+int int_div(int x, int y);
+int int_mod(int x, int y);
+int int_mul(int x, int y);
--- src/integer.c
+++ src/integer.c
@@ -1,1 +1,5 @@
 #include <integer.h>
+
+int int_add(int x, int y) {
+	return x + y;
+}
