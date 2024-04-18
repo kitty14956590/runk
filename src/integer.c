616f311373b6f11155bcc53cc3e0d6877330ee08
kitty14956590 <borbtrash@gmail.com>
2024-04-18 19:27:41 -0400

    unsigned instructions

--- src/integer.c
+++ src/integer.c
@@ -16,3 +16,28 @@ int int_div(int x, int y) {
 int int_mod(int x, int y) {
 	return x % y;
 }
+
+int int_mul(int x, int y) {
+	return x * y;
+}
+
+/* unsigned */
+unsigned int uint_add(unsigned int x, unsigned int y) {
+	return x + y;
+}
+
+unsigned int uint_sub(unsigned int x, unsigned int y) {
+	return x - y;
+}
+
+unsigned int uint_div(unsigned int x, unsigned int y) {
+	return x / y;
+}
+
+unsigned int uint_mod(unsigned int x, unsigned int y) {
+	return x % y;
+}
+
+unsigned int uint_mul(unsigned int x, unsigned int y) {
+	return x * y;
+}
