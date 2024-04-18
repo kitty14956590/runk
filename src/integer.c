362be29f4716d7432a0ac4ad3ee1d76df760c280
kitty14956590 <borbtrash@gmail.com>
2024-04-18 19:25:36 -0400

    add stuff

--- src/integer.c
+++ src/integer.c
@@ -1,5 +1,18 @@
 #include <integer.h>
 
+/* super complicated functions */
 int int_add(int x, int y) {
 	return x + y;
+}
+
+int int_sub(int x, int y) {
+	return x - y;
+}
+
+int int_div(int x, int y) {
+	return x / y;
+}
+
+int int_mod(int x, int y) {
+	return x % y;
 }
