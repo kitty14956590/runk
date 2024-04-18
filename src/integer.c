30c581cb7add22804e91c52a140996ef593338a4
kitty14956590 <borbtrash@gmail.com>
2024-04-18 19:37:31 -0400

    runk

--- src/integer.c
+++ src/integer.c
@@ -1,6 +1,6 @@
 #include <integer.h>
 
-/* super complicated functions */
+/* dword sized */
 int int_add(int x, int y) {
 	return x + y;
 }
@@ -21,7 +21,7 @@ int int_mul(int x, int y) {
 	return x * y;
 }
 
-/* unsigned */
+/* unsigned dword */
 unsigned int uint_add(unsigned int x, unsigned int y) {
 	return x + y;
 }
@@ -39,5 +39,133 @@ unsigned int uint_mod(unsigned int x, unsigned int y) {
 }
 
 unsigned int uint_mul(unsigned int x, unsigned int y) {
+	return x * y;
+}
+
+/* word sized */
+short int16_add(short x, short y) {
+	return x + y;
+}
+
+short int16_sub(short x, short y) {
+	return x - y;
+}
+
+short int16_div(short x, short y) {
+	return x / y;
+}
+
+short int16_mod(short x, short y) {
+	return x % y;
+}
+
+short int16_mul(short x, short y) {
+	return x * y;
+}
+
+/* unsigned word */
+unsigned short uint16_add(unsigned short x, unsigned short y) {
+	return x + y;
+}
+
+unsigned short uint16_sub(unsigned short x, unsigned short y) {
+	return x - y;
+}
+
+unsigned short uint16_div(unsigned short x, unsigned short y) {
+	return x / y;
+}
+
+unsigned short uint16_mod(unsigned short x, unsigned short y) {
+	return x % y;
+}
+
+unsigned short uint16_mul(unsigned short x, unsigned short y) {
+	return x * y;
+}
+
+
+/* byte sized */
+char int8_add(char x, char y) {
+	return x + y;
+}
+
+char int8_sub(char x, char y) {
+	return x - y;
+}
+
+char int8_div(char x, char y) {
+	return x / y;
+}
+
+char int8_mod(char x, char y) {
+	return x % y;
+}
+
+char int8_mul(char x, char y) {
+	return x * y;
+}
+
+/* unsigned byte */
+unsigned char uint8_add(unsigned char x, unsigned char y) {
+	return x + y;
+}
+
+unsigned char uint8_sub(unsigned char x, unsigned char y) {
+	return x - y;
+}
+
+unsigned char uint8_div(unsigned char x, unsigned char y) {
+	return x / y;
+}
+
+unsigned char uint8_mod(unsigned char x, unsigned char y) {
+	return x % y;
+}
+
+unsigned char uint8_mul(unsigned char x, unsigned char y) {
+	return x * y;
+}
+
+
+/* qword sized */
+long long int int64_add(long long int x, long long int y) {
+	return x + y;
+}
+
+long long int int64_sub(long long int x, long long int y) {
+	return x - y;
+}
+
+long long int int64_div(long long int x, long long int y) {
+	return x / y;
+}
+
+long long int int64_mod(long long int x, long long int y) {
+	return x % y;
+}
+
+long long int int64_mul(long long int x, long long int y) {
+	return x * y;
+}
+
+/* unsigned qword */
+unsigned long long int uint64_add(unsigned long long int x, unsigned long long int y) {
+	return x + y;
+}
+
+unsigned long long int uint64_sub(unsigned long long int x, unsigned long long int y) {
+	return x - y;
+}
+
+unsigned long long int uint64_div(unsigned long long int x, unsigned long long int y) {
+	return x / y;
+}
+
+unsigned long long int uint64_mod(unsigned long long int x, unsigned long long int y) {
+	return x % y;
+}
+
+unsigned long long int uint64_mul(unsigned long long int x, unsigned long long int y) {
 	return x * y;
 }
