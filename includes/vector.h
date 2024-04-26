dffb91c55ef0ce9b794e24952d768eff35b5ed26
kitty14956590 <borbtrash@gmail.com>
2024-04-26 11:22:49 -0400

    runk

--- includes/vector.h
+++ includes/vector.h
@@ -53,3 +53,56 @@ typedef struct {
 	long double z;
 	long double w;
 } vect4d_float128_t;
+
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+	float w;
+	float v;
+} vect5d_float32_t;
+
+typedef struct {
+	double x;
+	double y;
+	double z;
+	double w;
+	double v;
+} vect5d_float64_t;
+
+typedef struct {
+	long double x;
+	long double y;
+	long double z;
+	long double w;
+	long double v;
+} vect5d_float128_t;
+
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+	float w;
+	float v;
+	float u;
+} vect6d_float32_t;
+
+typedef struct {
+	double x;
+	double y;
+	double z;
+	double w;
+	double v;
+	double u;
+} vect6d_float64_t;
+
+typedef struct {
+	long double x;
+	long double y;
+	long double z;
+	long double w;
+	long double v;
+	long double u;
+} vect6d_float128_t;
