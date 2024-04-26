e89512044d1ff662e23b28421ec807901d2cde2c
kitty14956590 <borbtrash@gmail.com>
2024-04-26 11:19:29 -0400

    runk

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Ronald's Universal Number Kounter
 
-The best way to calculation math!
+The best way to calculate math!
 
 https://twitter.com/6thgrade4ever/status/1433519577892327424
--- src/float.c
+++ src/float.c
@@ -1,5 +1,59 @@
 #include <float.h>
 
+
+
+long double runk_float128_add(long double x, long double y) {
+    return x + y;
+
+}
+
+long double runk_float128_sub(long double x, long double y) {
+    return x - y;
+
+}
+
+long double runk_float128_div(long double x, long double y) {
+    return x / y;
+
+}
+
+long double runk_float128_mod(long double x, long double y) {
+    return x % y;
+
+}
+
+long double runk_float128_mul(long double x, long double y) {
+    return x * y;
+}
+
+double runk_float64_add(double x, double y) {
+    return x + y;
+}
 double runk_float64_sub(double x, double y) {
     return x - y;
+}
+double runk_float64_div(double x, double y) {
+    return x / y;
+}
+double runk_float64_mod(double x, double y) {
+    return x % y;
+}
+double runk_float64_mul(double x, double y) {
+    return x * y;
+}
+
+float runk_float32_add(float x, float y) {
+    return x + y;
+}
+float runk_float32_sub(float x, float y) {
+    return x - y;
+}
+float runk_float32_div(float x, float y) {
+    return x / y;
+}
+float runk_float32_mod(float x, float y) {
+    return x % y;
+}
+float runk_float32_mul(float x, float y) {
+    return x * y;
 }
