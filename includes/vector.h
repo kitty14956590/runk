d6a3093c6806591ea214815090f26d3ed98f1dfe
kitty14956590 <borbtrash@gmail.com>
2024-04-26 11:11:55 -0400

    victor

--- includes/vector.h
+++ includes/vector.h
@@ -1,5 +1,55 @@
 typedef struct {
 	float x;
 	float y;
-	float x;
+	float z;
 } vect3d_float32_t;
+
+typedef struct {
+	double x;
+	double y;
+	double z;
+} vect3d_float64_t;
+
+typedef struct {
+	long double x;
+	long double y;
+	long double z;
+} vect3d_float128_t;
+
+
+typedef struct {
+	float x;
+	float y;
+} vect2d_float32_t;
+
+typedef struct {
+	double x;
+	double y;
+} vect2d_float64_t;
+
+typedef struct {
+	long double x;
+	long double y;
+} vect2d_float128_t;
+
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+	float w;
+} vect4d_float32_t;
+
+typedef struct {
+	double x;
+	double y;
+	double z;
+	double w;
+} vect4d_float64_t;
+
+typedef struct {
+	long double x;
+	long double y;
+	long double z;
+	long double w;
+} vect4d_float128_t;
